51ffb1a47a449e80bad74b8f8c42c9e34eeab894
salimjon10 <salimjonrahimov10@mail.ru>
2024-09-13 10:46:48 +0700

    lab1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,90 @@
+package org.example;
+
+//1//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public Main() {
+    }
+
+    public static String fizzbuzz(int number) {
+        if (number % 5 == 0 && number % 7 == 0) {
+            return "fizzbuzz";
+        } else if (number % 5 == 0) {
+            return "fizz";
+        } else {
+            return number % 7 == 0 ? "buzz" : Integer.toString(number);
+        }
+    }
+
+    //    2
+    public static String reverseStr(String string) {
+        String result = "";
+
+        for(int index = string.length() - 1; index >= 0; --index) {
+            result = result + string.charAt(index);
+        }
+
+        return result;
+    }
+//3
+
+    public static double[] quadraticEquation(double a, double b, double c) throws Exception {
+        double D = b * b - 4.0 * a * c;
+        double[] x;
+
+        if (a == 0) {
+            throw new Exception("Its not quadratic equation");
+        }
+
+        if (D > 0.0) {
+            x = new double[]{(-b + Math.sqrt(D)) / (2.0 * a), (-b - Math.sqrt(D)) / (2.0 * a)};
+            return x;
+        } else if (D == 0.0) {
+            x = new double[]{-b / (2.0 * a)};
+            return x;
+        } else {
+            throw new Exception("There are no real roots");
+        }
+    }
+
+    //    4
+    public static double sumSerias() {
+        double n = 2.0;
+        double sum = 0.0;
+        double memberSerias;
+
+        do {
+            memberSerias = 1.0 / (n * n + n - 2.0);
+            sum += memberSerias;
+            n += 1.0;
+        } while(memberSerias >= Math.pow(10.0, -6.0));
+
+        return sum;
+    }
+
+    //    5
+    public static boolean polindrom(String string) {
+        int half;
+        string = string.toLowerCase();
+
+        if (string.length() % 2 == 0) {
+            half = string.length() / 2;
+        } else {
+            half = (string.length() - 1) / 2;
+        }
+
+        for (int index = 0; index < half; ++index) {
+            if (string.charAt(index) != string.charAt(string.length() - 1 - index)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int number = 1; number <= 500; ++number) {
+            System.out.println(fizzbuzz(number));
+        }
+    }
+}
